648bd882ef3ce977a3c389f49a5e8ba5a14cc91b
Leah <elloworldt@gmail.com>
2021-11-28 07:21:04 -0800

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+cats.push("Ralph")
+}
+
+function destructivelyPrependCat(name) {
+cats.unshift("Milo")
+}
+
+function destructivelyRemoveLastCat() {
+cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+cats.shift()
+}
+
+let copyOfcats() = [...cats];
+
+function appendCat(string){
+    copyOfcats.push("Sylvester");
+}
+
+function prependCat(name){
+    console.log(cats.slice(0));
+}
+
+function removeLastCat(){
+    
+}
+
+function removeFirstCat(){
+    
+}
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
